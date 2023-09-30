4ef9ac3bf839165638dee25d7d9588102c1ae2b0
Suvash Neupane <suvashlucid@gmail.com>
2023-09-30 08:17:14 +0545

    Some changes

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import React,{useState} from 'react';
-const namedd="suvash"
+import React, { useState } from 'react';
+
+const namedd = "suvash";
+
 function App() {
-    
+  const [named, setNamed] = useState(`Hi, I will do welcome using ${namedd} after clicking the click me.`);
 
- const[named,setnamed]=useState(`Hi I will do welcome using ${namedd} after clicking the click me.`)
- 
-
- 
-
- function checking()
-  {    
+  function checking() {
+    // Toggle the state between two different values
+     (named === `Hi, I will do welcome using ${namedd} after clicking the click me.`) ? setNamed(`Good!! Now You are Welcomed By ${namedd}Now`)
      
-    setnamed(`Good!! Now You are Welcomed By ${namedd}Now`)
+      :setNamed(`Hi, I will do welcome using ${namedd} after clicking the click me.`)
+    
   }
+
   return (
     <>
-        
       <div style={{ backgroundColor: "red" }}>
         <h1>{named}</h1>
         <button className="btN" onClick={checking}>Click me</button>
@@ -23,7 +22,5 @@ function App() {
     </>
   );
 }
-
-
 
 export default App;
